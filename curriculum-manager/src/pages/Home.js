4ee1307408edc826929b9607c29d01b46e87970c
Renata Nunes <renatanunes231@yahoo.com.br>
2022-04-16 20:04:03 -0300

    functionality to list curriculums implemented

--- curriculum-manager/src/pages/Home.js
+++ curriculum-manager/src/pages/Home.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return <div>Home</div>;
+  const [listCurriculum, setListCurriculum] = useState([]);
+
+  useEffect(() => {
+    const curriculums = JSON.parse(localStorage.getItem('curriculums'));
+    setListCurriculum(curriculums);
+  }, []);
+
+  return (
+    <div>
+      <h2>Currículos cadastrados</h2>
+      <section>
+        { listCurriculum.map(({ name, id }) => <Link key={id} to={`/view/${id}`}>{name}</Link>) }
+      </section>
+    </div>
+  );
 }
 
 export default Home;
